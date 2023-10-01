023498cb5a0e7405325be0768ea665854229262c
infoprofrafael <infoprofrafael@gmail.com>
2023-09-30 23:40:16 -0300

    Herois e classificações

---
+++ index.js
@@ -0,0 +1,36 @@
+let nameHero = 'Ralf';
+
+let xpHero = '9999';
+var textHero = "na classificação de HEROI nível:"
+let classHero = ""
+if(xpHero<=1000){
+    classHero="Ferro";
+    console.log(`O heroi ${nameHero} está ${textHero} ${classHero}`)
+} else if(xpHero>1001 && xpHero<=2000){
+    classHero="Bronze";
+    console.log(`O heroi ${nameHero} está ${textHero} ${classHero}`)
+} else if(xpHero>2001 && xpHero<=5000){
+    classHero="Prata";
+    console.log(`O heroi ${nameHero} está ${textHero} ${classHero}`)
+} else if(xpHero>5001 && xpHero<=6000){
+    classHero="Diamante";
+    console.log(`O heroi ${nameHero} está ${textHero} ${classHero}`)
+} else if(xpHero>6001 && xpHero<=7000){
+    classHero="Ouro";
+    console.log(`O heroi ${nameHero} está ${textHero} ${classHero}`)
+}else if(xpHero>7001 && xpHero<=8000){
+    classHero="Platina";
+    console.log(`O heroi ${nameHero} está ${textHero} ${classHero}`)
+}else if(xpHero>8001 && xpHero<=9000){
+    classHero="Ascendente";
+    console.log(`O heroi ${nameHero} está ${textHero} ${classHero}`)
+} else if(xpHero>9001 && xpHero<=10000){
+    classHero="Imortal";
+    console.log(`O heroi ${nameHero} está ${textHero} ${classHero}`)
+} else if(xpHero>10001){
+    classHero="Radiante";
+    console.log(`O heroi ${nameHero} está ${textHero} ${classHero}`)
+}  
+
+
+
